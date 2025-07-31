32a14706f46af56f2f092e1b43b10741050df0a3
Eric <180332937+StubbyGuy@users.noreply.github.com>
2025-07-31 20:53:29 +0000

    package and read.me update

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-// javascript
 const core = require('@actions/core');
+const core = require('@actions/github');
 
-const name = core.getInput('name');
-const output_value = 'Hello ${name}!';
-core.setOutput('greeting', output_value);
+try {
+    const name = core.getInput('name');
+    const output_value = 'Hello ${name}!';
+    core.setOutput('greeting', output_value);
+} catch (error) {
+    core.setFailed(error.message);
+}
+
 
 
